a841632439591f2502f2ea3cb76a411557f41ed0
Unknownplanet40 <ryanjamesc4@gmail.com>
2025-11-23 09:57:21 +0800

    Disable development mode by setting devmode to false

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { BackgroundMusic, SoundEffectSetup, InventorySetup, hintrecipeData } fro
 if (typeof $ === "undefined") {
   throw new Error("jQuery is required for this script to work. Please include jQuery before this script.");
 }
-let devmode = true;
+let devmode = false;
 let UNDER_DEVELOPMENT = false;
 let ENABLE_BACKGROUND_ANIMATION = true;
 let BACKGROUND_ROTATION_SPEED = 0.001;
